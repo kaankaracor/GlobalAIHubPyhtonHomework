6545057e21de43f80afb6947f5ecdf548ce5b313
kaan <kaankaracor13@gmail.com>
2020-12-22 01:18:26 +0300

    githuba proje gönderiyorum

---
+++ hw1.py
@@ -0,0 +1,11 @@
+i = 0
+while(i<2):
+    a = input("Please enter someting:")
+    i+=1
+    print("Type {} Value {}".format(type(a),a))
+
+j = 0
+while(j<3):
+    b = int(input("Please enter a number:"))
+    j+=1
+    print(f"Type {type(b)} Value {b}")
